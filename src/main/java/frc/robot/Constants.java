d8924882b82d7153b8f828c0e6fd27764d3f8cd0
GraysonE2228 <graysonetter@gmail.com>
2026-01-19 21:12:17 -0500

    Skeleton Code
    Added empty files and length and width, go me.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,6 +18,9 @@ public final class Constants {
   public static final Mode simMode = Mode.SIM;
   public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
 
+
+  public static final double robotWidth = 0.7112; // 28in
+  public static final double robotLength = 0.6858; // 27in
   public static enum Mode {
     /** Running on a real robot. */
     REAL,
---
+++ src/main/java/frc/robot/subsystems/kitchenAid/KitchenAid.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.kitchenAid;
+
+public class KitchenAid {
+    
+}
