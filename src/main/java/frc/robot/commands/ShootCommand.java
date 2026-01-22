535274b67fa8c787227d8764f162fb483a797aa4
GraysonE2228 <graysonetter@gmail.com>
2026-01-21 21:39:11 -0500

    Added Commands
    shoot command only one with code
    The rest are skeletons

---
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -0,0 +1,39 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.hopper.Hopper;
+import frc.robot.subsystems.turret.Turret;
+
+public class ShootCommand extends Command{
+
+    private final Hopper hopper;
+    private final Turret turret; 
+    private boolean initialized = false;   
+
+    public ShootCommand(Hopper hopper, Turret turret) {
+        this.hopper = hopper;
+        this.turret = turret;
+        addRequirements(hopper);
+    }
+
+    @Override
+    public void initialize() {}
+    
+
+    @Override
+    public void execute () {
+        if (turret.isAimed()) {
+            turret.enableshooter(true);
+        }
+    }
+
+    @Override 
+    public void end(boolean interrupted) {
+        turret.enableshooter(false);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
