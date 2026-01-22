4a13a265541a81e90565b7a4b50524e3c618a977
Perodactyl <friedwhoman@gmail.com>
2026-01-21 21:39:12 -0500

    implement hopper as talonFX

--- src/main/java/frc/robot/subsystems/hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/hopper/Hopper.java
@@ -1,5 +1,25 @@
 package frc.robot.subsystems.hopper;
 
 public class Hopper {
-    
+	private final HopperIO io;
+
+	private boolean on = false;
+
+	public Hopper(HopperIO io) {
+		this.io = io;
+	}
+
+	public boolean isOn() {
+		return this.on;
+	}
+
+	public void turnOn() {
+		this.on = true;
+		this.io.turnOn();
+	}
+	
+	public void turnOff() {
+		this.on = false;
+		this.io.turnOff();
+	}
 }
