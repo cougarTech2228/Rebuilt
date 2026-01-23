a0f1c027a159fda270f78add2b78144d2850a06b
rswanCougarTech <richhyswan@gmail.com>
2026-01-22 22:41:32 -0500

    1/22 input+io updates

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -2,4 +2,12 @@ package frc.robot.subsystems.intake;
 
 public class IntakeConstants {
     
+    public static final double idolVoltage = 0.0;
+    public static final double intakeVoltage = 0.0;
+    public static final double spitVoltage = 0.0;
+    
+    // Better to do voltage or position set?
+    public static final double homeVoltage = 0.0;
+    public static final double deployVoltage = 0.0;
+    
 }
