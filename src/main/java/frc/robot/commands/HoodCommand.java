cd1723b9a2903276bb047ad3dd76cf6dd67d458b
GraysonE2228 <graysonetter@gmail.com>
2026-01-24 13:04:49 -0500

    Intake Command
    Also hood command started

--- src/main/java/frc/robot/commands/HoodCommand.java
+++ src/main/java/frc/robot/commands/HoodCommand.java
@@ -1,5 +1,17 @@
-package frc.robot.commands;
+ package frc.robot.commands;
+import frc.robot.subsystems.turret.Turret;
+import edu.wpi.first.wpilibj2.command.Command;
 
-public class HoodCommand {
-    
+public class HoodCommand extends Command{
+    private final Turret turret;
+     
+
+
+    public HoodCommand (Turret turret, double hoodAngle) {
+        this.turret = turret;
+        
+        
+        addRequirements(turret);
+    }
+
 }
