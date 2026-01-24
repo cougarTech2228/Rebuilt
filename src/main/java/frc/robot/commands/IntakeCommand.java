2afba3c865d7fe1d9d0ea70f2ee1b7dd0ccb9d4d
GraysonE2228 <graysonetter@gmail.com>
2026-01-24 13:04:49 -0500

    Intake Command
    Also hood command started

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -1,5 +1,45 @@
 package frc.robot.commands;
 
-public class IntakeCommand {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.hopper.Hopper;
+import frc.robot.subsystems.intake.Intake;
+
+public class IntakeCommand extends Command{
+
+    private final Intake intake;
+    private final Hopper hopper;
+    private boolean initialized = false; 
     
+    public IntakeCommand(Intake intake, Hopper hopper) {
+        this.intake = intake;
+        this.hopper = hopper;
+        }    
+
+    @Override
+    public void initialize() {
+        if(hopperIsDeployed = false) {
+            hopper.deployHopper();
+        }
+    }
+
+    @Override
+    public void execute () {
+        intake.enableIntake();
+        hopper.enableHopper();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override 
+    public void end(boolean interrupted) {
+        intake.disableIntake();
+        hopper.disableHopper();
+
+        if (hopperIsDeployed == true && safeToRetract() == true) {
+            hopper.retractHopper();
+        }
+    }
 }
